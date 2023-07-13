2f90c2de1f300b4f2fbb136ee132fc6e6f23c726
we-z <yousefsalem00@gmail.com>
2023-07-12 20:52:26 -0400

    Initial Commit

---
+++ endlessfaller/endlessfallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  endlessfallerApp.swift
+//  endlessfaller
+//
+//  Created by Wheezy Salem on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct endlessfallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
