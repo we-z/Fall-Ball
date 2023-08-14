013d008c8e14c99e0cdebb9a318dc8fcc7a272f9
we-z <yousefsalem00@gmail.com>
2023-08-14 14:57:05 -0400

    Added Gif capabilities

--- endlessfaller/AnimationsView.swift
+++ endlessfaller/AnimationsView.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import WebKit
 
-struct AnimationsView: View {    
+struct GifImage: UIViewRepresentable {
+    private let name: String
+
+    init(_ name: String) {
+        self.name = name
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        let url = Bundle.main.url(forResource: name, withExtension: "gif")!
+        let data = try! Data(contentsOf: url)
+        webView.load(
+            data,
+            mimeType: "image/gif",
+            characterEncodingName: "UTF-8",
+            baseURL: url.deletingLastPathComponent()
+        )
+        webView.scrollView.isScrollEnabled = false
+
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.reload()
+    }
+
+}
+
+
+struct AnimationsView: View {
+    @State var isAnimating: Bool = true
     var body: some View {
-        VStack{
-            Spacer()
-            SwiftUIXmasTree2()
+        ZStack{
+            Color.orange
+            GifImage("pokeball")
         }
     }
 }
