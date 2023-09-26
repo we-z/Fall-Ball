65b6d0bd70fc2e85718fe36a4bfe86c032886d59
we-z <yousefsalem00@gmail.com>
2023-09-26 15:02:41 -0700

    Geame center leaderboard view

---
+++ FallBallWatchOS Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FallBallWatchOS Watch App
+//
+//  Created by Wheezy Salem on 9/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ endlessfaller/Views/GameCenterLeaderboardView.swift
@@ -0,0 +1,18 @@
+//
+//  GameCenterLeaderboardView.swift
+//  Fall Ball
+//
+//  Created by Wheezy Salem on 9/26/23.
+//
+
+import SwiftUI
+
+struct GameCenterLeaderboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameCenterLeaderboardView()
+}
