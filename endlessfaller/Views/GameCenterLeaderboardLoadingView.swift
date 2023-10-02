cbdaa1a1671559994457b6222c2527a30ab9e38f
we-z <yousefsalem00@gmail.com>
2023-10-01 22:56:57 -0700

    New leaderboard ui with game center

---
+++ endlessfaller/Views/GameCenterLeaderboardLoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  GameCenterLeaderboardLoadingView.swift
+//  Fall Ball
+//
+//  Created by Wheezy Salem on 9/30/23.
+//
+
+import SwiftUI
+
+struct GameCenterLeaderboardLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameCenterLeaderboardLoadingView()
+}
