bbb6d00f3a343763c156370b5bba73f01bfc65ef
we-z <yousefsalem00@gmail.com>
2023-10-06 00:22:06 -0700

    Make Plaque sharable

---
+++ endlessfaller/Views/ShareView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareView.swift
+//  Fall Ball
+//
+//  Created by Wheezy Salem on 10/5/23.
+//
+
+import SwiftUI
+
+struct ShareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareView()
+}
