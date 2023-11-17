cf6b9abb52e872e35f2a4088d16c026f3c8a28aa
we-z <yousefsalem00@gmail.com>
2023-11-17 10:53:13 -0800

    notification fix

--- endlessfaller/Configs/AppModel.swift
+++ endlessfaller/Configs/AppModel.swift
@@ -671,6 +671,7 @@ class NotificationManager: ObservableObject {
         //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
 
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.removeAllPendingNotificationRequests()
         center.add(request)
     }
     
