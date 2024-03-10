2923afff82729fc159c5d12e08250d0d22889aad
we-z <yousefsalem00@gmail.com>
2024-03-09 17:46:44 -0800

    add cardPassAnimation

--- endlessfaller/Views/LevelsToPassPlayerView.swift
+++ endlessfaller/Views/LevelsToPassPlayerView.swift
@@ -11,7 +11,22 @@ import GameKit
 struct LevelsToPassPlayerView: View {
     @ObservedObject var gameCenter = GameCenter.shared
     @ObservedObject private var appModel = AppModel.sharedAppModel
-
+    @State var cardXoffset = 0.0
+    @State var cardYoffset = 0.0
+    
+    func cardPassAnimation() {
+        withAnimation(.linear(duration: 0.3)) {
+            cardXoffset = 100
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            cardYoffset = -150
+            cardXoffset = 0
+            withAnimation(.linear(duration: 0.3)) {
+                cardYoffset = 0
+            }
+        }
+    }
+    
     var body: some View {
         let todaysPlayersList = gameCenter.todaysPlayersList
         VStack{
@@ -42,10 +57,12 @@ struct LevelsToPassPlayerView: View {
                         .frame(width: 120)
                         .padding()
                         .padding(.top, 36)
+                        .offset(x: cardXoffset, y: cardYoffset)
                         .onChange(of: appModel.score) { newScore in
                             if newScore >= todaysPlayersList[gameCenter.nextPlayerIndex].score {
                                 if gameCenter.nextPlayerIndex > 0 {
                                     print("nextPlayerIndex should be modified")
+                                    cardPassAnimation()
                                     gameCenter.nextPlayerIndex -= 1
                                 }
                             }
