c8af6dc0c19ff4ae5230a1fe0cadfb96bfee57a8
we-z <yousefsalem00@gmail.com>
2024-03-10 18:33:23 -0700

    reward animation and code completed

--- endlessfaller/Views/TempView.swift
+++ endlessfaller/Views/TempView.swift
@@ -10,30 +10,22 @@ import Vortex
 
 struct TempView: View {
 
-    var body: some View {
-        VortexView(colourTrail()) {
-            Circle()
-                .fill(.white)
-                .frame(width: 32)
-                .tag("circle")
+        @State private var isAnimating = false
+
+        var body: some View {
+            ZStack {
+                ForEach(0..<5, id: \.self) { index in
+                    BoinsView()
+                        .offset(y: -50)
+                        .rotationEffect(.degrees(Double(index) / Double(5) * 360))
+                }
+            }
+            .rotationEffect(.degrees(isAnimating ? 360 : 0))
+            .animation(Animation.linear(duration: 3).repeatForever(autoreverses: false), value: isAnimating)
+            .onAppear {
+                self.isAnimating = true
+            }
         }
-        
-    }
-    
-    func colourTrail() -> VortexSystem {
-        let system = VortexSystem(tags: ["circle"])
-        system.speed = 0.9
-        system.birthRate = 300
-        system.shape = .box(width: 0.15, height: 0.1)
-        system.angleRange = .degrees(3)
-        system.size = 0.3
-        system.colors = .random(.blue, .red, .yellow, .green)
-        system.sizeVariation = 0.6
-        system.lifespan = 0.3
-        system.sizeMultiplierAtDeath = 0.1
-        return system
-    }
-    
 }
 
 struct TempView_Previews: PreviewProvider {
