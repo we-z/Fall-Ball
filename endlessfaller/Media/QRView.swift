0b5c8f02f32a7f0a725509d3b8627c0f3a0a90e9
we-z <yousefsalem00@gmail.com>
2024-03-29 08:36:00 -0700

    new sticker design

--- endlessfaller/Media/QRView.swift
+++ endlessfaller/Media/QRView.swift
@@ -11,33 +11,39 @@ struct QRView: View {
     var body: some View {
         ZStack{
             LinearGradient(gradient: Gradient(colors: [.pink,.purple,.blue]), startPoint: UnitPoint(x: 0, y: 0.6), endPoint: UnitPoint(x: 0.6, y: 0.2))
-            Text("Download")
-                .bold()
-                .italic()
-                .font(.largeTitle)
-                .customTextStroke()
-                .scaleEffect(1.5)
-                .offset(y: -140)
-            Text("FALL BALL")
-                .bold()
-                .italic()
-                .font(.largeTitle)
-                .customTextStroke()
-                .scaleEffect(1.5)
-                .offset(y: 139)
-            ShockedBall()
-                .offset(x:106)
-                .scaleEffect(1.5)
-            FallBallLaughBall()
-                .offset(x:-106)
-                .scaleEffect(1.5)
             RotatingSunView()
-            Image("fallballQR")
-                .resizable()
-                .frame(width: 210, height: 210)
-                .cornerRadius(30)
-    
-            
+            Circle()
+                .stroke(lineWidth: 6)
+                .frame(width: 424)
+            VStack(spacing: 240){
+                Text("Download")
+                    .bold()
+                    .italic()
+                    .font(.largeTitle)
+                    .customTextStroke(width: 1.5)
+                    .scaleEffect(1.5)
+                Text("FALL BALL")
+                    .bold()
+                    .italic()
+                    .font(.largeTitle)
+                    .customTextStroke(width: 1.5)
+                    .scaleEffect(1.5)
+            }
+            HStack(spacing: 270) {
+                ShockedBall()
+                    .scaleEffect(1.5)
+                FallBallLaughBall()
+                    .scaleEffect(1.5)
+            }
+            ZStack{
+                Rectangle()
+                    .frame(width:224, height: 224)
+                Image("fallballQR")
+                    .resizable()
+                    .frame(width: 210, height: 210)
+                    .cornerRadius(30)
+            }
+            .cornerRadius(39)
         }
         .mask(
             Circle()
