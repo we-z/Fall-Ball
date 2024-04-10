3877d15b7f25981a0102711761933c0185c2e5cf
we-z <yousefsalem00@gmail.com>
2024-04-09 22:40:50 -0700

    buff up Swipe up to\ncontinue text

--- endlessfaller/Views/ContentView.swift
+++ endlessfaller/Views/ContentView.swift
@@ -80,7 +80,7 @@ struct ContentView: View {
                                 .italic()
                                 .multilineTextAlignment(.center)
                                 .frame(height: 300)
-                                .customTextStroke()
+                                .customTextStroke(width: 2.1)
                                 .scaleEffect(1.5)
                                 
                             }
