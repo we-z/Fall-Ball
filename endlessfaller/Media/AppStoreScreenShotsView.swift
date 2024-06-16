b7f14329b888ccca90715a6ab040fdab0e6c582b
we-z <yousefsalem00@gmail.com>
2024-06-16 14:20:05 -0700

    new AppStoreScreenShotsView

--- endlessfaller/Media/AppStoreScreenShotsView.swift
+++ endlessfaller/Media/AppStoreScreenShotsView.swift
@@ -13,23 +13,33 @@ struct AppStoreScreenShotsView: View {
             LinearGradient(gradient: Gradient(colors: [.pink,.purple,.blue]), startPoint: UnitPoint(x: 0, y: 0.6), endPoint: UnitPoint(x: 0.6, y: 0.2))
                 .ignoresSafeArea()
             RotatingSunView()
-                .offset(y:-120)
+                .offset(y: -90)
             VStack{
-                Text("FALL BALL")
-                    .font(.largeTitle)
+                Text("Fall Ball")
+                    .font(.system(size: 100))
                     .bold()
                     .italic()
-                    .customTextStroke(width: 1.5)
+                    .customTextStroke(width: 3.3)
+                    
+                DollarsignBallView()
+                    .scaleEffect(7)
+                    .padding(90)
+                VStack{
+                    VStack{
+                        Text("Swipe up \nto play!")
+                            .italic()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                        Image(systemName: "arrow.up")
+                    }
+                    .bold()
+                    .font(.largeTitle)
                     .scaleEffect(1.5)
-                ZStack{
-                    Image("homescreenshot")
-                        .resizable()
-                        .frame(width: 230, height: 500)
-                        .cornerRadius(30)
-                    Image("iPhoneBezel")
-                        .resizable()
-                        .frame(width: 270, height: 540)
+                    SwipeUpHand()
+                        .offset(x: 60, y:60)
                 }
+                .frame(width: 300, height: 400)
+                .customTextStroke(width: 2.7)
             }
             .offset(y:30)
         }
