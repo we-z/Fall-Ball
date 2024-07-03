c8dfdebf5f60bb2033a79c317195986c82370084
we-z <yousefsalem00@gmail.com>
2024-07-02 22:54:22 -0700

    make strategy mode easier and more fun

--- endlessfaller/Configs/AppModel.swift
+++ endlessfaller/Configs/AppModel.swift
@@ -152,12 +152,12 @@ class AppModel: ObservableObject {
         if userPersistedData.strategyModeEnabled {
             let m1 = -0.0004004004004004004
             let c1 = 0.5004004004004003
-            let fastest = (m1 * Double(difficultyInput) + c1) * 1.8
+            let fastest = (m1 * Double(difficultyInput) + c1) * 2.1
             print("fastest: \(fastest)")
             
             let m2 = -0.0012012012012012011
             let c2 = 1.5012012012012013
-            let slowest = (m2 * Double(difficultyInput) + c2) * 1.8
+            let slowest = (m2 * Double(difficultyInput) + c2) * 2.1
             print("slowest: \(slowest)")
             
             self.ballSpeed = Double.random(in: fastest...slowest)
